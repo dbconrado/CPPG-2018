14df32db5405bd64676b617a90761860aea5fd77
Jose Luiz <joseluiz98.jl@gmail.com>
2018-05-22 18:52:21 -0300

    Queries are nested and parsed through view, bug fix

--- controller/routes.js
+++ controller/routes.js
@@ -18,6 +18,15 @@ router.get('/users/detail', function(req, res, next) {
 });
 
 router.get('/charts', function(req, res, next) {
+	var sql;
+	var ultimoAno=0;
+	var penultimoAno=0;
+	var anoRetrasado=0;
+	var nPesquisaUltimoAno = 0;
+	var nEnsinoUltimoAno = 0;
+	var nExtensaoUltimoAno = 0;
+
+
 	var con = mysql.createConnection({
 	  host: "localhost",
 	  user: "root",
@@ -28,16 +37,84 @@ router.get('/charts', function(req, res, next) {
 
 	try {
 		con.connect();
+		con.query("SELECT anoEdital FROM projeto GROUP BY anoEdital ORDER BY anoEdital DESC", function (er, result, fields)
+		{
+		if (er) throw er;
+		else
+		{
+			//setYearValues(result);
 
-		var sql = "SELECT anoEdital FROM projeto GROUP BY anoEdital ORDER BY anoEdital DESC;SELECT COUNT(*) AS totalPesquisa FROM projeto WHERE categoriaProjeto = 'Pesquisa' GROUP BY anoEdital;SELECT COUNT(*) AS totalExtensao FROM projeto WHERE categoriaProjeto = 'Extensão' GROUP BY anoEdital;SELECT COUNT(*) AS totalEnsino FROM projeto WHERE categoriaProjeto = 'Ensino' GROUP BY anoEdital;";
-		con.query(sql, [1,2,3,4], function (er, result, fields) {
-			if (er) throw er;
-			else {
-			    sendToView(result[0][0].anoEdital, result[0][1].anoEdital, result[0][2].anoEdital,
-			   	result[0][0].totalPesquisa,result[1][0].totalExtensao,result[2][0].totalEnsino);
-			}
+			ultimoAno = result[0].anoEdital;
+			penultimoAno = ultimoAno-1;
+			anoRetrasado = penultimoAno-1;
+
+			sql = "SELECT COUNT(*) AS total, anoEdital, categoriaProjeto FROM projeto WHERE categoriaProjeto = 'Pesquisa' GROUP BY anoEdital ORDER BY anoEdital DESC;";
+			con.query(sql, function (er, result, fields)
+			{
+				if(er) throw er;
+				else
+				{
+					//console.log(result);
+					if (result[0] != undefined)
+					{
+						if(result[0].anoEdital == ultimoAno) nPesquisaUltimoAno = result[0].total;
+						else nPesquisaUltimoAno = 0;
+					}
+					else nPesquisaUltimoAno = 0;
+					//console.log(nPesquisaUltimoAno);
+
+					sql = "SELECT COUNT(*) AS total, anoEdital, categoriaProjeto FROM projeto WHERE categoriaProjeto = 'Ensino' GROUP BY anoEdital ORDER BY anoEdital DESC;";
+					con.query(sql, function (er, result, fields)
+					{
+						if(er) throw er;
+						else
+						{
+							//console.log(result);
+							if (result[0] != undefined)
+							{
+								if(result[0].anoEdital == ultimoAno) nEnsinoUltimoAno = result[0].total;
+								else nEnsinoUltimoAno = 0;
+							}
+							else nEnsinoUltimoAno = 0;
+							//console.log(nEnsinoUltimoAno);
+
+
+							sql = "SELECT COUNT(*) AS total, anoEdital, categoriaProjeto FROM projeto WHERE categoriaProjeto = 'Extensão' GROUP BY anoEdital ORDER BY anoEdital DESC;";
+							con.query(sql, function (er, result, fields)
+							{
+								if(er) throw er;
+								else
+								{
+									//console.log(result);
+									if (result[0] != undefined)
+									{
+										if(result[0].anoEdital == ultimoAno) nExtensaoUltimoAno = result[0].total;
+										else nExtensaoUltimoAno = 0;
+										console.log(nExtensaoUltimoAno);
+										/*console.log(ultimoAno);
+										console.log(penultimoAno);
+										console.log(anoRetrasado);
+										console.log(nPesquisaUltimoAno);
+										console.log(nEnsinoUltimoAno);
+										console.log(nExtensaoUltimoAno);*/
+										sendToView(ultimoAno, penultimoAno, anoRetrasado, nPesquisaUltimoAno, nExtensaoUltimoAno, nEnsinoUltimoAno);
+									}
+									else nExtensaoUltimoAno = 0;
+									//console.log(nExtensaoUltimoAno);
+								}
+							});
+						}
+					});
+				}
+			});
+		}
 		});
-		con.end();
+
+		/*function setYearValues(years) {
+			ultimoAno = years[0].anoEdital;
+			penultimoAno = years[1].anoEdital;
+			anoRetrasado = years[2].anoEdital;
+		}*/
 
 		function sendToView(ultimoAno, penultimoAno, anoRetrasado, totalPesquisaUltimoAno, totalExtensaoUltimoAno, totalEnsinoUltimoAno) {
 			res.render(path.resolve(__dirname + '/../views/index.ejs'), {
