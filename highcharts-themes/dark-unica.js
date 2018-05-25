1ae59b41db079c0879a435b6ec1aa06060a7ce8e
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-05-25 17:53:06 -0300

    Now it's possible to download themes and use in charts, dark-unica is the example of an dark theme used in this commit

--- highcharts-themes/dark-unica.js
+++ highcharts-themes/dark-unica.js
@@ -7,6 +7,15 @@
  * @author Torstein Honsi
  */
 
+'use strict';
+/* global document */
+// Load the fonts
+Highcharts.createElement('link', {
+    href: 'https://fonts.googleapis.com/css?family=Unica+One',
+    rel: 'stylesheet',
+    type: 'text/css'
+}, null, document.getElementsByTagName('head')[0]);
+
 Highcharts.theme = {
     colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
         '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
