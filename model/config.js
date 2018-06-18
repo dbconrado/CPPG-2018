b7c3dc933005077d3d0197c4c07dcc90d2499ecf
Jose Luiz <joseluiz98.jl@gmail.com>
2018-06-18 16:21:16 -0300

    minor fix

--- model/config.js
+++ model/config.js
@@ -7,7 +7,7 @@ var config = {
             host:   'localhost',
             port:   '3306',
             user:   'root',
-            pass:   '',
+            pass:   'root',
             db:     'cppg'
         },
         //server details
