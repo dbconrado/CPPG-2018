db6fa93c9612a4b10f357efcf66ed974f5a16c7e
José Luiz <joseluiz98.jl@gmail.com>
2018-07-24 10:18:51 -0300

    teste

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
 var routes = require('./controller/routes');
-var compression = require('compression');
 
 var app = express();
 app.use(compression());
