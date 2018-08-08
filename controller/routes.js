296b2b6c976184904bf329b7e54082f4080c1c38
Jose Luiz <joseluiz98.jl@gmail.com>
2018-08-08 15:23:10 -0300

    chart containing students assistances distributed over the years is now showed on homepage

--- controller/routes.js
+++ controller/routes.js
@@ -7,13 +7,96 @@ var sort = require('srtr');
 var vars = require('../model/variables.js');
 var proceeding = require('../model/functions.js');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next){
 	try
 	{
+		if(vars.con.state === 'disconnected'){
+			vars.con.connect(function(err) {
+				if (err) throw err;
+			});
+		}
+
+		getAssistanceChartData();
+	}
+	catch(e)
+	{
+		throw e;
+	}
+
+	function lookForProjectsPerYear(actualYear, countYears, typesOfAssistance)
+	{
+		var sql = "SELECT AP.modalidadeBolsa AS tipoBolsa FROM aluno_participa_projeto AP JOIN projeto P ON P.idProjeto = AP.idProjeto WHERE P.anoEdital = ?";
+
+		typesOfAssistance[countYears] = [0, 0, 0, 0, 0];
+		
+		return new Promise(function(resolve,reject)
+		{
+			vars.con.query(sql, [actualYear], function (er, result, fields)
+			{
+				if(er) return reject(er);
+				for(var j = 0; j<result.length; j++)
+				{
+					if(result[j].tipoBolsa == "PIBIC")
+					{
+						typesOfAssistance[countYears][0]++;
+					}
+					else if(result[j].tipoBolsa == "PIBIC-JR")
+					{
+						typesOfAssistance[countYears][1]++;
+					}
+					else if(result[j].tipoBolsa == "PIBIT")
+					{
+						typesOfAssistance[countYears][2]++;
+					}
+					else if(result[j].tipoBolsa == "PIBEX")
+					{
+						typesOfAssistance[countYears][3]++;
+					}
+					else
+					{
+						typesOfAssistance[countYears][4]++;
+					}
+				}
+				resolve(typesOfAssistance);
+			});
+		});
+	}
+
+		function getCol(matrix, col)
+		{
+			var column = [];
+			for(var i=0; i<matrix.length; i++){
+				column.push(matrix[i][col]);
+			}
+			return column;
+		 }
+
+		function sendToView(years, typesOfAssistance)
+		{
+			var pibicAssistance 	= getCol(typesOfAssistance, 0);
+			var pibicJrAssistance 	= getCol(typesOfAssistance, 1);
+			var pibitAssistance 	= getCol(typesOfAssistance, 2);
+			var pibexAssistance 	= getCol(typesOfAssistance, 3);
+			var volunteerAssistance = getCol(typesOfAssistance, 4);
+
+			var yearData = [];
+			for(var i=(years.length-1); i>=0; i--)
+			{
+				yearData.push(years[i].anoEdital);
+			}
+
 		getTeacherCloud().then(function(cloud)
 		{
 			tagCloud.tagCloud(cloud, function (err, data) {
-				res.render('index', { cloud: data } );
+				res.render('index', {
+					cloud: data,
+					years: yearData,
+					pibicAssistance: pibicAssistance,
+					pibicJrAssistance: pibicJrAssistance,
+					pibitAssistance: pibitAssistance,
+					pibexAssistance: pibexAssistance,
+					volunteerAssistance: volunteerAssistance
+				});
 			}, {
 				classPrefix: 'btn tag tag',
 				randomize: false,
@@ -22,11 +105,7 @@ router.get('/', function(req, res, next) {
 				additionalAttributes: {href: vars.config.url +'/keyword={{tag}}'}
 			});
 		}).catch((err) => setImmediate(() => { throw err; }));
-	}
-	catch(err)
-	{
-		throw err;
-	}
+		}
 
 	function getTeacherCloud()
 	{
@@ -54,6 +133,39 @@ router.get('/', function(req, res, next) {
 		{
 			throw err;
 		}
+	}
+
+	function getAssistanceChartData()
+	{
+		// Recupera os três últimos anos nos quais hajam dados no banco
+		var sql = "SELECT anoEdital FROM aluno_participa_projeto AP JOIN projeto P ON P.idProjeto = AP.idProjeto GROUP BY P.anoEdital ORDER BY P.anoEdital DESC";
+
+		vars.con.query(sql, function (er, result, fields)
+		{
+			if (er) throw er;
+			else
+			{
+				var years = result;
+				var numberOfYears = 0;
+				var conta=[];
+				var typesOfAssistance = [];
+
+				for(var i=0; i<years.length; i++)
+				{
+					lookForProjectsPerYear(years[i].anoEdital, numberOfYears, typesOfAssistance).then(function()
+					{
+						conta.push(1);
+
+						// Se já rodei todas as promises, mando pra view
+						if(conta.length == years.length)
+						{
+							sendToView(years,typesOfAssistance);
+						}
+					}).catch((err) => setImmediate(() => { throw err; }));
+					numberOfYears++;
+				}
+			}
+		});
 	}
 });
 
@@ -290,128 +402,5 @@ router.get('/pub-discentes/compilados/2014-2016.pdf', function(req, res, next) {
 			}
 		}
 	});
-});
-
-router.get('/stackedBar', function(req, res, next) {
-	var sql;
-	var years;
-
-	try {
-		if(vars.con.state === 'disconnected'){
-			vars.con.connect(function(err) {
-				if (err) throw err;
-			});
-		}
-		
-		// Recupera os três últimos anos nos quais hajam dados no banco
-		sql = "SELECT anoEdital FROM aluno_participa_projeto AP JOIN projeto P ON P.idProjeto = AP.idProjeto GROUP BY P.anoEdital ORDER BY P.anoEdital DESC";
-
-		vars.con.query(sql, function (er, result, fields)
-		{
-		if (er) throw er;
-		else
-		{
-			years = result;
-			var numberOfYears = 0;
-			var conta=[];
-			var typesOfAssistance = [];
-
-			for(var i=0; i<years.length; i++)
-			{
-				lookForProjectsPerYear(years[i].anoEdital, numberOfYears, typesOfAssistance).then(function(typesDown) {
-					conta.push(1);
-
-					// Se já rodei todas as promises, mando pra view
-					if(conta.length == years.length)
-					{
-						sendToView(years, typesOfAssistance);
-					}
-				}).catch((err) => setImmediate(() => { throw err; }));
-				numberOfYears++;
-			}
-		}
-		});
-	}
-	catch(e) {
-		throw e;
-	}
-
-	function lookForProjectsPerYear(actualYear, countYears, typesOfAssistance)
-		{
-			var sql = "SELECT AP.modalidadeBolsa AS tipoBolsa FROM aluno_participa_projeto AP JOIN projeto P ON P.idProjeto = AP.idProjeto WHERE P.anoEdital = ?";
-
-			typesOfAssistance[countYears] = [0, 0, 0, 0, 0];
-			
-			return new Promise(function(resolve,reject)
-			{
-				vars.con.query(sql, [actualYear], function (er, result, fields)
-				{
-					if(er) return reject(er);
-					for(var j = 0; j<result.length; j++)
-					{
-						if(result[j].tipoBolsa == "PIBIC")
-						{
-							typesOfAssistance[countYears][0]++;
-						}
-						else if(result[j].tipoBolsa == "PIBIC-JR")
-						{
-							typesOfAssistance[countYears][1]++;
-						}
-						else if(result[j].tipoBolsa == "PIBIT")
-						{
-							typesOfAssistance[countYears][2]++;
-						}
-						else if(result[j].tipoBolsa == "PIBEX")
-						{
-							typesOfAssistance[countYears][3]++;
-						}
-						else
-						{
-							typesOfAssistance[countYears][4]++;
-						}
-					}
-					resolve(typesOfAssistance);
-				});
-			});
-		}
-
-		function getCol(matrix, col)
-		{
-			var column = [];
-			for(var i=0; i<matrix.length; i++){
-				column.push(matrix[i][col]);
-			}
-			return column;
-		 }
-
-		function sendToView(years, typesOfAssistance)
-		{
-			var pibicAssistance 	= getCol(typesOfAssistance, 0);
-			var pibicJrAssistance 	= getCol(typesOfAssistance, 1);
-			var pibitAssistance 	= getCol(typesOfAssistance, 2);
-			var pibexAssistance 	= getCol(typesOfAssistance, 3);
-			var volunteerAssistance = getCol(typesOfAssistance, 4);
-
-			var yearData = [];
-			for(var i=(years.length-1); i>=0; i--)
-			{
-				yearData.push(years[i].anoEdital);
-			}
-
-			res.render('stackedBar.ejs', {
-				years: yearData,
-				pibicAssistance: pibicAssistance,
-				pibicJrAssistance: pibicJrAssistance,
-				pibitAssistance: pibitAssistance,
-				pibexAssistance: pibexAssistance,
-				volunteerAssistance: volunteerAssistance
-			}, function(err, html) {
-				if(err) {
-					throw err;
-				} else {
-					res.send(html);
-				}
-			});
-		}
 });
 module.exports = router;
