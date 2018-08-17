3804938016bd637555cd2b7b918750857cb8a327
José Luiz <joseluiz98.jl@gmail.com>
2018-08-17 18:09:29 -0300

    started to implement all options select/deselect on years range combobox

--- public/js/geraCertificado.js
+++ public/js/geraCertificado.js
@@ -1,5 +1,35 @@
 $(document).ready(function()
 {
+    $("#selYear").on('change', function()
+    {
+        console.log($('#selYear:checked'));
+        if ($('#selYear:checked').length == $('#selYear').length)
+        {
+            console.log("cai22");
+        }
+
+        var id = $(this).find("option:selected").attr("id");
+        console.log(id);
+        if(id == "allOptions")
+        {
+            var allSelected = $("#selectID option:not(:selected)").length == 1;
+            console.log(allSelected);
+            if(allSelected)
+            {
+                console.log("cai1");
+                $('#idSelect option').attr("selected",false);
+                $('#idSelect').selectpicker('refresh');
+            }
+            else
+            {
+                console.log("cai2");
+                $('#selYear option').attr("selected","selected");
+                $('#allOptions').text("Nenhuma das opções")
+                $('#selYear').selectpicker('refresh');
+            }
+        }
+    });
+
     $("#selTeacher").on('change', function()
     {
         var data = {};
@@ -18,10 +48,11 @@ $(document).ready(function()
                 
                 if(min)
                 {
-                    $('#selYearMin').empty();
+                    $('#selYear').empty();
+                    $("#selYear").append("<option data-tokens= 'Todos os anos' id='allOptions'>Todos os anos</option>");
                     for(var i = min; i<=max; i++)
                     {
-                        $("#selYearMin").append("<option data-tokens= '" + i + "'> " + i + " </option>");	
+                        $("#selYear").append("<option data-tokens= '" + i + "'> " + i + " </option>");	
                     }
                     $('.selectpicker').selectpicker('refresh');
                     $('#selectMinYear').show();
