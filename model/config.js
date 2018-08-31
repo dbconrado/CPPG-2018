2850de7ecb3e01918b742b64c0446013f8dfb5ed
José Luiz <joseluiz98.jl@gmail.com>
2018-08-31 14:56:56 -0300

    database port fix

--- model/config.js
+++ model/config.js
@@ -21,8 +21,8 @@ var config = {
         url: 'http://200.18.128.48/CPPG',
         //database connection settings
         database: {
-            host: '127.0.0.1',
-            port: '27017',
+            host: 'localhost',
+            port: '3306',
             user:   'root',
             pass:   'root',
             db:     'cppg'
