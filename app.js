177251a6b42b3056bb8999eae3b136be44be227b
José Luiz <joseluiz98.jl@gmail.com>
2018-08-31 14:49:12 -0300

    Now, express-mysql-server is used to session-management in production.

--- app.js
+++ app.js
@@ -4,12 +4,36 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressSession = require('express-session');
 var routes = require('./controller/routes');
 var compression = require('compression');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+var vars = require('./model/variables.js');
 
 var app = express();
 app.use(compression());
+
+// cookies and session setup
+app.use(cookieParser());
+
+var options = {
+  host: vars.config.database.host,
+  port:vars.config.database.port,
+  user: vars.config.database.user,
+  password: vars.config.database.pass,
+  database: vars.config.database.db
+};
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+  key: 'session_cookie_name',
+  secret: 'session_cookie_secret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +47,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
-app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', routes);
 
