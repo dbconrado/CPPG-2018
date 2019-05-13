245d9ac9eccaaa9abc642937e58c59d3be6f0ae6
Valtensir Lopes <valtensirl@gmail.com>
2019-05-13 17:01:56 -0300

    filter implementation in proceedings page

--- public/assets/js/searchProceedings.js
+++ public/assets/js/searchProceedings.js
@@ -1,21 +1,41 @@
 $(document).ready(function () {
 
-	$('.star').on('click', function () {
-      $(this).toggleClass('star-checked');
-    });
+  $('.star').on('click', function () {
+    $(this).toggleClass('star-checked');
+  });
 
-    $('.ckbox label').on('click', function () {
-      $(this).parents('tr').toggleClass('selected');
-    });
+  $('.ckbox label').on('click', function () {
+    $(this).parents('tr').toggleClass('selected');
+  });
 
-    $('.btn-filter').on('click', function () {
-      var $target = $(this).data('target');
-      if ($target != 'all') {
-        $('.table tr').css('display', 'none');
-        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
-      } else {
-        $('.table tr').css('display', 'none').fadeIn('slow');
-      }
-    });
+  $('.btn-filter').on('click', function () {
+    var $target = $(this).data('target');
+    if ($target != 'all') {
+      $('.table tr').css('display', 'none');
+      $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+    } else {
+      $('.table tr').css('display', 'none').fadeIn('slow');
+    }
+  });
 
- });
+  $('.filter').click(function () {
+    var value = $(this).text();
+
+    if (value != 'Todos') {
+      $('tr').each(function () {
+        if ($(this).attr('id') == value) {
+          $(this).fadeIn();
+        } else {
+          $(this).fadeOut();
+        }
+      });
+      $('.filter').css('box-shadow','none');
+      $(this).css('box-shadow','0 0 0 .2rem rgba(0,123,255,.25)');
+    } else {
+      $('tr').fadeIn();
+      $('.filter').css('box-shadow','none');
+      $(this).css('box-shadow','0 0 0 .2rem rgba(0,123,255,.25)');
+    }
+  });
+
+});
